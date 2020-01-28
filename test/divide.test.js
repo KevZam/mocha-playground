af7066a251395cf4a967c1dd8ec92d7aafd3250d
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-01-27 20:41:52 -0800

    added describe function

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,1 +1,11 @@
 const divide = require("../index");
+
+describe("Divide function", () => {
+  it("should divide positive integers correctly", () => {
+    // spec code here
+  });
+
+  it("should throw an error when divide by zero", () => {
+    // spec code here
+  });
+});

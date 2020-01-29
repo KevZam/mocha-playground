f249437d4eaaa595dfdf0b20c10f893f14944a49
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-01-28 20:49:44 -0800

    added tests

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ function divide(a, b) {
     throw new Error("Cannot divide by 0");
   }
 
-  return a / b;
+  return a + b;
 }
+
+module.exports = divide;
+
+function sort(list) {
+  for (let i = 2; i < list.length; i++) {
+    let j = i;
+    while (j > 0 && list[j - 1] > list[j]) {
+      let temp = list[j];
+      list[j] = list[j - 1];
+      list[j - 1] = temp;
+      j--;
+    }
+  }
+  return list;
+}
+
+const list = [5, 4, 3, 2, 1];
+sort(list);
--- test/divide.test.js
+++ test/divide.test.js
@@ -1,11 +1,28 @@
+const expect = require("chai").expect;
 const divide = require("../index");
 
-describe("Divide function", () => {
-  it("should divide positive integers correctly", () => {
-    // spec code here
-  });
+// describe("Divide function", () => {
+//   it("should divide positive integers correctly", () => {
+//     expect(divide(8, 4)).to.equal(2);
+//   });
 
-  it("should throw an error when divide by zero", () => {
-    // spec code here
+//   it("should throw an error when divide by zero", () => {
+//     expect(() => {
+//       divide(8, 0);
+//     }).to.throw();
+//   });
+// });
+
+describe("sort function", () => {
+  it("should return an array in ascending order", () => {
+    expect(() => {
+      sort([5, 4, 3, 2, 1]);
+    }).to.deep.equal([1, 2, 3, 4, 5]);
+  });
+  it("first number should not be greater than second", () => {
+    expect(() => {
+      let arr1 = [5, 4, 3, 2, 1];
+      let sortedNums = sort(arr1);
+    });
   });
 });
